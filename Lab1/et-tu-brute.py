b0ade4554a995b64ba7eb99ef2f59634852a9974
CDees <bcd021@latech.edu>
2026-03-19 12:31:32 -0500

    Outline incomplete Caesar algorithm

--- Lab1/et-tu-brute.py
+++ Lab1/et-tu-brute.py
@@ -14,24 +14,38 @@ import sys
 ALPHABET = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQ
               RSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;
               :'\",<.>/?"""
-              
-
 # debug for test printing 
-DEBUG = False 
+DEBUG = True 
 
 
-### MAIN ### 
-   
+# -- taking input from StdIn -- 
+def take_input():
+    userInput: str = sys.stdin.read()
+    if(DEBUG):
+        print(userInput)
 
-# taking input from StdIn 
-userInput: str = sys.stdin.read()
-print(userInput)
-
-# trying several different shifts
-
+# -- performing a caesar cipher shift --
+# for character at index i in ALPHABET, character = 
+# char at index i + shift 
+# def caesar_shift(shift_amount: int): 
+    # alphabet_length: int = len(ALPHABET) 
+    # for index, character in enumerate(ALPHABET): 
+        # # mod to account for wrapped index 
+        # wrapped_index = index % alphabet_length 
+        # # using standard index 
+        # if(index + shift_amount < alphabet_length): 
+            # character[index] = character[index + shift_amount] 
+        # # otherwise, using wrapped index 
+        # else:
+            # character[wrapped_index] = character[wrapped_index + shift_amount] 
+           
 
 # determine which results are valid by comparing word amount to
 # dictionary text file 
 
 
-
+### MAIN ### 
+take_input()
+caesar_shift(1) 
+print(ALPHABET)
+   
