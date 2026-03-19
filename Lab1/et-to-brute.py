7dbe05fe7aae361b33ce8a441358989e71019e95
CDees <bcd021@latech.edu>
2026-03-19 10:27:46 -0500

    Initial formatting

---
+++ Lab1/et-to-brute.py
@@ -0,0 +1,34 @@
+# Et tu, Brute?
+#
+# Team Name: Encryptodes
+# Members: Barry Dees, Niko Krause, Javen Wilson, Steven Alleman
+#          Isaiah Hinds
+#
+# A program that attempts to solve cipher texts that are 
+# shifted by some number.
+
+
+# the alphabet
+ALPHABET = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQ
+              RSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;
+              :'\",<.>/?"""
+              
+
+# debug for test printing 
+DEBUG = false 
+
+
+### MAIN ### 
+   
+
+# taking input from StdIn 
+
+
+# trying several different shifts
+
+
+# determine which results are valid by comparing word amount to
+# dictionary text file 
+
+
+
