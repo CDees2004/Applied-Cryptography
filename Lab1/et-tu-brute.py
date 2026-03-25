146d5bd1347d08c5e6058803fe9c0bc0402104e8
CDees <bcd021@latech.edu>
2026-03-25 13:46:16 -0500

    Implement caesar cipher algorithm

--- Lab1/et-tu-brute.py
+++ Lab1/et-tu-brute.py
@@ -1,19 +1,19 @@
 # Et tu, Brute?
 #
 # Team Name: Encryptodes
-# Members: Barry Dees, Niko Krause, Javen Wilson, Steven Alleman
-#          Isaiah Hinds
+# Members: Barry Dees, Niko Krause, Javen Wilson,
+#          Steven Alleman, and Isiah Hinds.
 #
 # A program that attempts to solve cipher texts that are 
 # shifted by some number.
 
-#from sys import stdin
 import sys
 
 # the alphabet
 ALPHABET = """abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQ
               RSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;
               :'\",<.>/?"""
+              
 # debug for test printing 
 DEBUG = True 
 
@@ -24,28 +24,30 @@ def take_input():
     if(DEBUG):
         print(userInput)
 
-# -- performing a caesar cipher shift --
-# for character at index i in ALPHABET, character = 
-# char at index i + shift 
-# def caesar_shift(shift_amount: int): 
-    # alphabet_length: int = len(ALPHABET) 
-    # for index, character in enumerate(ALPHABET): 
-        # # mod to account for wrapped index 
-        # wrapped_index = index % alphabet_length 
-        # # using standard index 
-        # if(index + shift_amount < alphabet_length): 
-            # character[index] = character[index + shift_amount] 
-        # # otherwise, using wrapped index 
-        # else:
-            # character[wrapped_index] = character[wrapped_index + shift_amount] 
-           
 
-# determine which results are valid by comparing word amount to
-# dictionary text file 
+# -- performing a caesar cipher shift --
+
+# take in alphabet list and shift amount, return shifted alphabet 
+def caesar_shift(alphabet: str, shift_amount: int) ->str: 
+    alphabet_length: int = len(ALPHABET) 
+    result: list[str] = []
+    
+    for index in range(alphabet_length): 
+        # using mod to account for wrapping 
+        wrapped_index = (index + shift_amount) % alphabet_length 
+        result.append(alphabet[wrapped_index])
+    
+    string_result: str = "".join(result) 
+    return string_result 
+    
+    
+# -- determine which results are valid by comparing word amount to
+#    dictionary text file --
 
 
 ### MAIN ### 
 take_input()
-caesar_shift(1) 
-print(ALPHABET)
+result_string: str = caesar_shift(ALPHABET, 1)
+print(result_string)
+
    
