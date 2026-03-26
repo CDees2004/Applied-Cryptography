adbf09692c0edbe68aaf6f3429c10ac91506c08a
CDees <bcd021@latech.edu>
2026-03-26 16:19:12 -0500

    Setup lab 2

--- Lab2/le-chiffre.py
+++ Lab2/le-chiffre.py
@@ -1,10 +1,11 @@
 # Le Chiffre
-# Team Name:
-# Members:
+#
+# Team Name: Encryptodes
+# Members:Barry Dees, Niko Krause, Javen Wilson,
+#         Steven Alleman, and Isiah Hinds.
+#
 # A program that attempts to solve cipher texts that are shifted by some number.
 # For the thresholds, we were able to get ciphertext-1= , ciphertext-2= , ciphertext-3= , ciphertext-4= , and the doubly encrypted=
-
-
 
 # the alphabet
 ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? "
