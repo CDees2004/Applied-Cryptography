0a093b1392658b420b94b8349041def805223f2c
CDees <bcd021@latech.edu>
2026-03-26 21:17:28 -0500

    Add standard input

--- Lab2/le-chiffre.py
+++ Lab2/le-chiffre.py
@@ -7,5 +7,17 @@
 # A program that attempts to solve cipher texts that are shifted by some number.
 # For the thresholds, we were able to get ciphertext-1= , ciphertext-2= , ciphertext-3= , ciphertext-4= , and the doubly encrypted=
 
+import sys 
+
 # the alphabet
 ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\\|;:'\",<.>/? "
+
+# debug for test printing 
+DEBUG = True 
+
+# -- taking input from StdIn -- 
+def take_input() ->str:
+    userInput: str = sys.stdin.read()
+    if(DEBUG):
+        print(userInput)
+    return userInput
