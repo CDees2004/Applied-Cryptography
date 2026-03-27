4731f971d11c55803fd4cd59d61226e55f19c12d
CDees <bcd021@latech.edu>
2026-03-27 13:44:46 -0500

    Establish simple framework

--- Lab2/le-chiffre.py
+++ Lab2/le-chiffre.py
@@ -5,7 +5,8 @@
 #         Steven Alleman, and Isiah Hinds.
 #
 # A program that attempts to solve cipher texts that are shifted by some number.
-# For the thresholds, we were able to get ciphertext-1= , ciphertext-2= , ciphertext-3= , ciphertext-4= , and the doubly encrypted=
+# For the thresholds, we were able to get ciphertext-1= , ciphertext-2= , 
+# ciphertext-3= , ciphertext-4= , and the doubly encrypted=
 
 import sys 
 
@@ -15,18 +16,24 @@ ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$
 # debug for test printing 
 DEBUG = True 
 
+
+# -- reading the contents of the dictionary file --
+with open("dictionary.txt", "r") as dictionary:
+    dictionary_content = dictionary.read()
+
+
 # -- taking input from StdIn -- 
 def take_input() ->str:
     userInput: str = sys.stdin.read()
     if(DEBUG):
         print(userInput)
     return userInput
-    
-    
-# -- reading the contents of the dictionary file --
-with open("dictionary.txt", "r") as dictionary:
-    dictionary_content = dictionary.read()
-    
-
-    
+  
+  
+# -- setting up the vigenere table    
+def map_alphabet() -> dict[str, int]: 
+    pass 
+   
+   
 ### MAIN ###
+user_input: str = take_input()
