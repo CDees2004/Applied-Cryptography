0aec21b4e595d67903090f78d533cf1ced0258b6
CDees <bcd021@latech.edu>
2026-05-14 21:22:40 -0500

    Complete lab 5

--- Lab5/rijndael.py
+++ Lab5/rijndael.py
@@ -12,8 +12,9 @@ from Crypto.Cipher import AES
 BLOCK_SIZE = 16
 
 # the padding character to use to make the plaintext a multiple of BLOCK_SIZE in length
-PAD_WITH = b"#"
+PAD_WITH = "#"
 
+PUNCTUATION = set("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
 
 # =====================================================
 # CHANGE THESE VALUES ONLY
@@ -21,23 +22,21 @@ PAD_WITH = b"#"
 
 # For ciphertext-1, ciphertext-2, ciphertext-3:
 DICTIONARY_FILE = "dictionary1-3.txt"
-#THRESHOLD = 
-#USE_TAG = 
-#BINARY_OUTPUT = 
-#START_FILTER = []
+THRESHOLD = 0.75
+BINARY_OUTPUT = False
+START_FILTER = []
 
 # For ciphertext-4, use:
-#DICTIONARY_FILE = "dictionary4.txt"
-#THRESHOLD = 
-#USE_TAG = 
-#BINARY_OUTPUT = 
-#START_FILTER = []
+#DICTIONARY_FILE = "dictionary4-1.txt" # THIS NAME MIGHT NEED TO BE CHANGED IN YOUR GRADING
+# THE TEMPLATE NAME DISAGREES WITH THE PROVIDED FILE NAME!!!!!
+#THRESHOLD = 0.75
+#BINARY_OUTPUT = True
+#START_FILTER = ['j', 'J']
 
 # For ciphertext-5, use:
 #DICTIONARY_FILE = "dictionary5.txt"
-#THRESHOLD = 
-#USE_TAG = 
-#BINARY_OUTPUT = 
+#THRESHOLD = 0.75
+#BINARY_OUTPUT = True
 #START_FILTER = []
 
 # =====================================================
@@ -46,47 +45,125 @@ DICTIONARY_FILE = "dictionary1-3.txt"
 
 # Reading the ciphertext from Stdin as bytes
 def take_input() -> bytes:
-    user_input:bytes = stdin.read().encode('utf-8', errors='surrogateescape')
-    return user_input
-    # return stdin.buffer.read()
+    # user_input:bytes = stdin.read().encode('utf-8', errors='surrogateescape')
+    # return user_input
+    return stdin.buffer.read()
     
 # Reading dictionary. Takes in file name and returns list of possible keys
 def generate_candidate_keys(DICTIONARY_FILE: str) -> list[str]:
     # Opening dictionary, reading, putting each line into a list
     candidate_keys: list[str] = []
    
-    with open(DICTIONARY_FILE, 'r') as dictionary_file:
+   # Using ignore to skip characters that don't fit the encoding
+    with open(DICTIONARY_FILE, 'r', encoding="utf-8", errors="ignore") as dictionary_file:
         for line in dictionary_file:
             candidate_key: str = line.strip()
-            candidate_keys.append(candidate_key)
+            # Filtering empty lines 
+            if candidate_key:
+                candidate_keys.append(candidate_key)
             
     return candidate_keys
     
-def rijndael_decryption(ciphertext: bytes, candidate_keys: list[str]):
-    # Repeating the algorihm for every candidate key
-    for key in candidate_keys:
-        # Convert candidate key to bytes and hashing
-        byte_key: bytes = key.encode() # Using bytes intead of bytearray to prevent side effects
-        
-        hash_object = sha256()
-        hash_object.update(byte_key)
-        hashed_key = hash_object.digest()
-        
-        # IV is the first 16 bytes of the ciphertext 
-        initialization_vector: bytes = ciphertext[:16]
-        
-        # E is the rest of the ciphertext 
-        e: bytearray = bytearray(ciphertext[16:])
+# Helper function to perform the AES-CBC-Decrypt
+def aes_cbc_decrypt(e, hashed_candidate_key, initialization_vector):
+    cipher = AES.new(hashed_candidate_key, AES.MODE_CBC, initialization_vector)
+    return cipher.decrypt(e)
+    
+# Helper function to normalize an individual word 
+def normalize_word(word: str) ->str:
+    normalized_word: str = ""
+    for character in word: 
+        if (character not in PUNCTUATION):
+            normalized_word += character.lower()
+            
+    return normalized_word
+    
+# Normalizing the candidate keys
+def normalize_dictionary(candidate_keys:str) ->set[str]:
+    normalized_candidates = set()
+    for word in candidate_keys:
+        normalized_candidates.add(normalize_word(word))
+    return normalized_candidates
 
-        # # Need to pad E to be a multiple of the block size 
-        while (len(e) % BLOCK_SIZE != 0):
-            e.extend(PAD_WITH)
+# Scoring the plaintext candidates to be compared against THRESHOLD
+def score_plaintexts(candidate_key: str, normalized_dictionary: list[str]):
+    # Splitting m into words 
+    split_m: str = candidate_key.split()
+    total: int = 0 
+    valid: int = 0
+    for word in split_m: 
+        normalized_word = normalize_word(word)
+        if not normalized_word:
+            # Skipping invalid words
+            continue
+        total += 1
+        if normalized_word in normalized_dictionary:
+            valid += 1
+    # These will be compared against the THRESHOLD
+    return valid, total
+
+# Checking if the expected output is a PDF 
+def check_PDF(m_bytes: bytes)-> bool:
+    # Returns a boolean based on if the header is found
+    return b"%PDF" in m_bytes[:20] or b"%PDF" in m_bytes
+    
+def rijndael_decryption(ciphertext: bytes, candidate_keys: list[str]):
+    # Outside of loop since these calculations are the same for all candidate keys
+    # IV is the first 16 bytes of the ciphertext 
+    initialization_vector: bytes = ciphertext[:16]
+    
+    # E is the rest of the ciphertext 
+    e: bytes = ciphertext[16:].strip(b"\r\n") # cleaning up invalid bytes
+
+    # Need to trim padding on E to be a multiple of the block size 
+    remainder_from_block_size: int = len(e) % BLOCK_SIZE
+    if (remainder_from_block_size != 0):
+        e = e[:-remainder_from_block_size] # Cutting the padding off
         
-        # Applying the AES decryption algorithm
-        cipher = AES.new(hashed_key, AES.MODE_CBC, initialization_vector)
+    # Normalizing the keys 
+    normalized_dictionary: set = normalize_dictionary(candidate_keys)
         
-        plaintext: bytes = cipher.decrypt(e)
-      
+    # Repeating the algorihm for every candidate key
+    for candidate_key in candidate_keys:
+        # If ciphertext-4 we can filter keys not starting with j/J
+        if (START_FILTER):
+            if candidate_key[0] not in START_FILTER:
+                continue
+                
+        # Conerting candidate key to bytes and hashing 
+        k = sha256(candidate_key.encode()).digest()
+ 
+        # Applying the AES decryption algorithm to get M
+        m: bytes = aes_cbc_decrypt(e, k, initialization_vector)
+        # Removing padding. Must encode the pad char!!
+        m = m.rstrip(PAD_WITH.encode())
+        
+        # If the expected output is readable text
+        if not BINARY_OUTPUT:
+            try:
+                # Convert m from bytes -> string 
+                m_string_rep: str = m.decode("utf-8", errors="ignore")
+            except:
+                # Skipping gibberish results
+                continue
+                
+            # Scoring the plaintext candidate
+            valid_words, total_words = score_plaintexts(m_string_rep, normalized_dictionary)
+            
+            # If the THRESHOLD is satisfied, the result is valid
+            if (valid_words / total_words) > THRESHOLD:
+                print(f"Key = {candidate_key}")
+                print(m_string_rep)
+                return
+            
+        # If the expected output is a PDF or binary file 
+        elif (BINARY_OUTPUT or check_PDF(m)):
+            # PRINTING FOR PDF AND BINARY, ASSIGNMENT DOES NOT MENTION HOW TO BINARY THAT ISNT PDF
+            stderr.write(f"Key = {candidate_key}\n")
+            # m as raw bytes to stdout
+            stdout.buffer.write(m)
+            return  
+            
 # =====================================================
 # MAIN
 # =====================================================
